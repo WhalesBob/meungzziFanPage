af3de4eee3aea2e3d48c90eca2cf2dd4fbe3dd62
WhalesBob <ndw8200@naver.com>
2022-05-24 23:37:13 +0900

    new Page open debug

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
 $(document).ready(function(){
+    let count = 0;
     $("#quokatalk").hide;
     $("#biebertalk").hide;
     $("#bieber").click(function(){
+        count++;
         $("#biebertalk").fadeIn(1000);
         $("#biebertalk").css('display','inline-block');
         $("#biebertalk").fadeOut(1000);
+        if(count == 2){
+            window.open("meungzziDay/1stDay.html");
+        }
     })
 
     $("#quoka").click(function(){
+        count++;
         $("#quokatalk").fadeIn(1000);
         $("#quokatalk").css('display','inline-block');
         $("#quokatalk").fadeOut(1000);
+        if(count == 2){
+            window.open("meungzziDay/1stDay.html");
+        }
     })
+
+    
 })
