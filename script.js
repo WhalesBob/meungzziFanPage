80c616f4611c3b9102fee08daeb6b187aa143415
WhalesBob <ndw8200@naver.com>
2022-05-25 09:40:47 +0900

    change opening method

--- script.js
+++ script.js
@@ -3,24 +3,20 @@ $(document).ready(function(){
     $("#quokatalk").hide;
     $("#biebertalk").hide;
     $("#bieber").click(function(){
-        count++;
         $("#biebertalk").fadeIn(1000);
         $("#biebertalk").css('display','inline-block');
         $("#biebertalk").fadeOut(1000);
-        if(count == 2){
-            window.open("meungzziDay/1stDay.html");
-        }
+        $("#biebertalk").click(() => { count++; opening(count);})
     })
 
     $("#quoka").click(function(){
-        count++;
         $("#quokatalk").fadeIn(1000);
         $("#quokatalk").css('display','inline-block');
         $("#quokatalk").fadeOut(1000);
-        if(count == 2){
-            window.open("meungzziDay/1stDay.html");
-        }
-    })
-
-    
+        $("#quokatalk").click(() => { count++; opening(count);});
+    })    
 })
+
+function opening(count){
+    if(count == 2){ window.open(window.open("meungzziDay/1stDay.html"));};
+}
